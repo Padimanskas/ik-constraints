20895c755c1ff46428bbb1a7e15c51510737c261
Stanislav Padimanskas <s.padimanskas@gmail.com>
2018-12-09 21:55:28 +0300

    source code beautified

--- src/main.js
+++ src/main.js
@@ -3,28 +3,28 @@ import IKChain from './chain.class';
 
 let chain = new IKChain(4, 100);
 let target = {
-  x: 0,
-  y: 0
+    x: 0,
+    y: 0
 };
 let mouse = {
-  x: 0,
-  y: 0
+    x: 0,
+    y: 0
 };
 
 chain.generate();
 
 function loop() {
-  target.x += mouse.x - target.x;
-  target.y += mouse.y - target.y;
+    target.x += mouse.x - target.x;
+    target.y += mouse.y - target.y;
 
-  chain.update(target);
-  chain.draw();
-  requestAnimationFrame(loop);
+    chain.update(target);
+    chain.draw();
+    requestAnimationFrame(loop);
 }
 
 loop();
 
-$('#canvas').mousemove(function(e) {
-  mouse.x = e.offsetX;
-  mouse.y = e.offsetY;
+$('#canvas').mousemove(function (e) {
+    mouse.x = e.offsetX;
+    mouse.y = e.offsetY;
 });
