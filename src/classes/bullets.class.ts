0e4e17a5cd19f74e1fd58cf6dfac8e6bf37b5a60
stanislau.padzimanskas <stanislau.padzimanskas@greyloud.com>
2019-11-11 12:18:33 +0300

    bullet class refactoring

--- src/classes/bullets.class.ts
+++ src/classes/bullets.class.ts
@@ -2,16 +2,21 @@ import render from '../utils/renderer'
 import utils from '../utils/utils'
 import Bullet from '../classes/bullet.class';
 
+/**
+ * Bullets factory class
+ */
 export default class Bullets {
 
-    private bullets: Array<Bullet> = [];
+    private bullets: Bullet[] = [];
 
     constructor() {
         render.pushToUpdate({
             update: () => {
                 this.bullets.forEach(bullet => {
                     bullet.update();
-                    this.removeBullet(bullet);
+                    if(this.checkBulletIsOutside(bullet)) {
+                        this.removeBullet(bullet);
+                    }
                 });
             }
         });
@@ -21,13 +26,14 @@ export default class Bullets {
         this.bullets.push(new Bullet(x, y, speed, angle));
     }
 
-    removeBullet(bullet: Bullet) {
-        let isOutsideBullet = utils.isOutOfScreen(bullet.getPosition());
-        if (isOutsideBullet) {
-            bullet.removeSprite();
-            let bulletId = this.bullets.indexOf(bullet);
-            this.bullets.splice(bulletId, 1);
-        }
+    checkBulletIsOutside(bullet: Bullet): boolean {
+        return utils.isOutOfScreen(bullet.getPosition());
+    }
+
+    removeBullet(bullet: Bullet): void {
+        bullet.removeSprite();
+        let bulletId = this.bullets.indexOf(bullet);
+        this.bullets.splice(bulletId, 1);
     }
 
 }
